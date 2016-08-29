b21ab7feaa37db1c427c9aa0d5f64379b8c532f2
Phillip Jones <phillipemersonjones@gmail.com>
2016-08-29 11:16:06 -0500

    added mixins

--- src/router.js
+++ src/router.js
@@ -104,7 +104,7 @@ export default Router.extend({
   renderPage (page, opts = {layout: true}) {
     if (opts.layout) {
       page = (
-        <Layout>
+        <Layout me={app.me}>
           {page}
         </Layout>
       )
